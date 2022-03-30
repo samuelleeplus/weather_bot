340c9f8dc278ebd27d61d89e2ff4b49cd3fe8416
Samuel Lee <samuelleplus@gmail.com>
2022-03-30 21:35:41 +0300

    added weather actions

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,56 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+from calendar import TextCalendar
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+import os, requests
+
+class ActionGetWeather(Action):
+
+    def name(self)-> Text:
+        return "action_get_weather"
+    
+
+    def run(self, dispatcher, tracker, domain):
+        loc = tracker.get_slot('GPE')
+        print(loc)
+        degree_sign= u'\N{DEGREE SIGN}'
+
+        # need to get weather API key from openweathermap.org
+        # and save to ENV variable
+        payload = {'q': loc, 'units':'metric', 'appid': os.environ['WEATHER_API_KEY']}
+        #http get method 
+        response = requests.get('http://api.openweathermap.org/data/2.5/find', params=payload)
+        
+        try:
+            # getting the data in JSON format
+            data = response.json()
+            print(data)
+            print(type(data))   
+            x = data['list'][0]
+
+            temperature = x['main']['temp']
+            description = x['weather'][0]['description']
+            city = x['name']
+            humidity = x['main']['humidity']
+            windSpeed = x['wind']['speed']
+            cloud = x['clouds']['all']
+            
+            weatherData = """In {}, it is {} at the moment at {}{}C. \nThe humidity level is: {}%, \nwind speed: {}m/s,\n cloudiness in the sky is {}%.""".format(city, description, degree_sign,temperature, humidity, windSpeed, cloud)
+            
+            
+            dispatcher.utter_message(weatherData)
+
+            return [SlotSet("GPE", None)]
+
+        except requests.exceptions.HTTPError as e:
+            dispatcher.utter_message(text="HTTP Error! City not found!")
+
+        except Exception as e:
+            dispatcher.utter_message(text="Could not find the city!")
+
